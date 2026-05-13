90e6db50f5d1454ac07e541da4d0ff9e23f1143f
Vladimir Zarypov <krre31@gmail.com>
2026-05-13 06:42:42 +0300

    Adabt creating backup script for different databases

--- m31/backup-db.sh
+++ m31/backup-db.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" &>/dev/null && pwd)
-BACKUP_DIR="$SCRIPT_DIR/ocean/backups"
+
+DB_NAME=$1
+
+if [ -z "$DB_NAME" ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+BACKUP_DIR="$SCRIPT_DIR/$DB_NAME/backups"
 mkdir -p "$BACKUP_DIR"
 
 DATE=$(date +%Y-%m-%d_%H-%M-%S)
 DB_CONTAINER="postgres"
 DB_USER="postgres"
-DB_NAME="ocean"
 
-docker exec $DB_CONTAINER pg_dump -U $DB_USER $DB_NAME | gzip > "$BACKUP_DIR/$DB_NAME.$DATE.sql.gz"
+set -o pipefail
 
-find "$BACKUP_DIR" -type f -mtime +30 -name "*.sql.gz" -delete
+docker exec $DB_CONTAINER pg_dump -U $DB_USER "$DB_NAME" | gzip -9 > "$BACKUP_DIR/$DB_NAME.$DATE.sql.gz"
+
+if [ $? -eq 0 ]; then
+    echo "[$DATE] Backup for '$DB_NAME' successful"
+else
+    echo "[$DATE] Backup for '$DB_NAME' failed" >&2
+    exit 1
+fi
+
+find "$BACKUP_DIR" -type f -mtime +10 -name "$DB_NAME.*.sql.gz" -delete
